6acb813bc2fe8835d532a6ed3c4e7bb5a2aa14ca
Ash Wilson <smashwilson@gmail.com>
2016-03-09 14:56:28 -0500

    Identify pull request builds.

--- worker.js
+++ worker.js
@@ -5,37 +5,62 @@ var entry = require('./lib/entry');
 
 module.exports = {
   init: function (config, job, context, callback) {
+    var isPullRequest = job.trigger.type === 'pull-request';
+
     callback(null, {
       env: {},
       path: [],
 
-      deploy: function (context, done) {
-        var write = function () {
-          var text = util.format.apply(null, arguments);
+      test: function (context, done) {
+        if (isPullRequest) {
+          var opts = assembleOptions(config, context, job.trigger.url);
 
-          if (text.substr(-1) !== '\n') {
-            text += '\n';
-          }
-
-          context.out(text);
-        };
-
-        var opts = {
-          root: context.dataDir,
-          dataContainer: process.env.STRIDER_WORKSPACE_CONTAINER,
-          contentServiceURL: config.contentServiceURL,
-          contentServiceAPIKey: config.contentServiceAPIKey,
-          contentServiceTLSVerify: config.contentServiceTLSVerify,
-          say: write,
-          whisper: function () {}
-        };
-
-        if (config.verbose) {
-          opts.whisper = write;
+          done(null, true);
+        } else {
+          done(null, false);
         }
+      },
+
+      deploy: function (context, done) {
+        var opts = assembleOptions(config, context);
 
         entry.recursivelyPrepare(opts, done);
       }
     });
   }
 };
+
+var makeWriter = function (context) {
+  return function () {
+    var text = util.format.apply(null, arguments);
+
+    if (text.substr(-1) !== '\n') {
+      text += '\n';
+    }
+
+    context.out(text);
+  };
+};
+
+var assembleOptions = function (config, context, pullRequestURL) {
+  var write = makeWriter(context);
+
+  var opts = {
+    root: context.dataDir,
+    dataContainer: process.env.STRIDER_WORKSPACE_CONTAINER,
+    pullRequestURL: pullRequestURL,
+    contentServiceURL: config.contentServiceURL,
+    contentServiceAPIKey: config.contentServiceAPIKey,
+    contentServiceTLSVerify: config.contentServiceTLSVerify,
+    stagingContextServiceURL: config.stagingContentServiceURL,
+    stagingContentServiceAdminAPIKey: config.stagingContentServiceAdminAPIKey,
+    say: write,
+    whisper: function () {}
+  };
+
+  if (config.verbose) {
+    opts.whisper = write;
+  }
+
+  return opts;
+}
