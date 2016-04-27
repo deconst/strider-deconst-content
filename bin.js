5e2d85fac6b5e2e3ff30545d0d56e249d3e12782
Ash Wilson <smashwilson@gmail.com>
2016-04-27 10:29:33 -0400

    BAM

--- bin.js
+++ bin.js
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
 
-var util = require('util');
-var entry = require('./lib/entry');
+var entry = require('./lib/entry')
 
 var toolbelt = {
-  workspacePath: function () { return '.' }
+  workspacePath: function () { return '.' },
 
   info: console.log,
   debug: console.log,
   error: console.error
-};
+}
 
 entry.recursivelyPrepare(toolbelt, null, function (err) {
   if (err) {
-    logging.logger.error("Preparation unsuccessful.", err);
+    toolbelt.error('Preparation unsuccessful.', err)
 
-    process.exit(1);
+    process.exit(1)
   }
-});
+})
